36b3a7c60aeba81ff624cf7301b38972d3a55ddd
easymean <m2162003@gmail.com>
2021-05-17 22:12:49 +0900

    feat: jpa audit

---
+++ emart2/src/main/java/com/example/emart2/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.emart2.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+  private boolean isActive;
+
+
+}
