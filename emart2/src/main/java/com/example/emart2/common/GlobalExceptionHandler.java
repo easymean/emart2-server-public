880fae159cba34141100cc42e4e6cdf4c00db606
easymean <m2162003@gmail.com>
2021-05-24 00:54:14 +0900

    chore: exception handler

--- emart2/src/main/java/com/example/emart2/common/GlobalExceptionHandler.java
+++ emart2/src/main/java/com/example/emart2/common/GlobalExceptionHandler.java
@@ -1,4 +1,7 @@
 package com.example.emart2.common;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 }
--- emart2/src/main/java/com/example/emart2/type/ErrorCode.java
+++ emart2/src/main/java/com/example/emart2/type/ErrorCode.java
@@ -3,6 +3,7 @@ package com.example.emart2.type;
 public enum ErrorCode {
   NOT_FOUND(1000, "NOT FOUND"),
   ACCESS_DENIED(1004, "ACCESS_DENIED"),
+  INVALID_PARAM(1005, "INVALID_PARAM")
   ;
   private final String message;
   private final int code;
