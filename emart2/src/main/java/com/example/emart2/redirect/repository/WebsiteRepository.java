15d000ce5b968ab157a97d3b55f6eb5095bf4746
easymean <m2162003@gmail.com>
2021-05-24 00:49:28 +0900

    feat: website api

---
+++ emart2/src/main/java/com/example/emart2/redirect/repository/WebsiteRepository.java
@@ -0,0 +1,16 @@
+package com.example.emart2.redirect.repository;
+
+import com.example.emart2.redirect.entity.Website;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface WebsiteRepository extends JpaRepository<Website, Long> {
+
+  Optional<Website> findByIdAndIsActiveIsTrue(Long id);
+
+  List<Website> findAllByIsActiveIsTrue();
+
+}
