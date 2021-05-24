d94d80016338eec995981f2c3149c8b354f424a9
easymean <m2162003@gmail.com>
2021-05-24 23:27:57 +0900

    fix: fix category mapper and controller

--- emart2/src/main/java/com/example/emart2/redirect/controller/CategoryController.java
+++ emart2/src/main/java/com/example/emart2/redirect/controller/CategoryController.java
@@ -7,38 +7,39 @@ import com.example.emart2.redirect.dto.CategoryResponseList;
 import com.example.emart2.redirect.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/category")
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
 
   CategoryService categoryService;
-  public CategoryController(CategoryService categoryService){
+
+  public CategoryController(CategoryService categoryService) {
     this.categoryService = categoryService;
   }
 
-
   @GetMapping("{id}")
-  public CommonResponse<CategoryResponse> getCategoryById(@PathVariable("id") Long id){
+  public CommonResponse<CategoryResponse> getCategoryById(@PathVariable("id") Long id) {
     return CommonResponse.ok("success", categoryService.findById(id));
   }
 
-  @GetMapping("/")
-  public CommonResponse<CategoryResponseList> getCategoryList(){
+  @GetMapping
+  public CommonResponse<CategoryResponseList> getCategoryList() {
     return CommonResponse.ok("success", categoryService.getList());
   }
 
-  @PostMapping("/")
-  public CommonResponse<CategoryResponse> createCategory(@RequestBody CategoryRequest request){
+  @PostMapping
+  public CommonResponse<CategoryResponse> createCategory(@RequestBody CategoryRequest request) {
     return CommonResponse.ok("success", categoryService.createCategory(request));
   }
 
   @PutMapping("{id}")
-  public CommonResponse<CategoryResponse> updateCategory(@PathVariable("id") Long id, @RequestBody CategoryRequest request){
+  public CommonResponse<CategoryResponse> updateCategory(@PathVariable("id") Long id, @RequestBody CategoryRequest request) {
     return CommonResponse.ok("success", categoryService.updateCategory(id, request));
   }
 
   @DeleteMapping("{id}")
-  public CommonResponse<Void> deleteCategory(@PathVariable("id") Long id){
+  public CommonResponse<Void> deleteCategory(@PathVariable("id") Long id) {
     categoryService.deleteCategory(id);
-    return CommonResponse.ok(null,null );
+    return CommonResponse.ok(null, null);
   }
 }
--- emart2/src/main/java/com/example/emart2/type/mapper/CategoryMapper.java
+++ emart2/src/main/java/com/example/emart2/type/mapper/CategoryMapper.java
@@ -4,17 +4,16 @@ import com.example.emart2.redirect.dto.CategoryRequest;
 import com.example.emart2.redirect.dto.CategoryResponse;
 import com.example.emart2.redirect.entity.Category;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
-  CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
-
   Category toEntity(CategoryRequest dto);
 
+  @Mapping(source = "colOrder", target = "order")
   CategoryResponse toDto(Category category);
 
   List<CategoryResponse> toDto(List<Category> list);
