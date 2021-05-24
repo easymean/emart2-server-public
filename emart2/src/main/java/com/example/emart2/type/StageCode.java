107f6f8c539bb16dace14ae72c96d679fdee30f8
easymean <m2162003@gmail.com>
2021-05-25 00:18:06 +0900

    fix: add dev/prod as stage code

--- emart2/src/main/java/com/example/emart2/type/StageCode.java
+++ emart2/src/main/java/com/example/emart2/type/StageCode.java
@@ -4,19 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum StageCode {
-  WEB_FRONT("프론트", "WEB_FRONT"),
-  WEB_BOS("보스", "WEB_BOS"),
-  DEPLOY("배포", "DEPLOY"),
-  ADMIN("관리","ADMIN"),
-  MONITORING("모니터링","MONITORING"),
-  ;
+  WEB_FRONT("프론트", "WEB_FRONT", 3),
+  WEB_BOS("보스", "WEB_BOS", 4),
+  DEPLOY("배포", "DEPLOY", 5),
+  ADMIN("관리", "ADMIN", 6),
+  MONITORING("모니터링", "MONITORING", 7),
+  DEV("개발", "DEV", 0),
+  PROD("운영", "PROD", 1);
 
   private final String title;
   private final String code;
+  private final int num;
 
-  StageCode(String title, String code){
+  StageCode(String title, String code, int num) {
     this.title = title;
     this.code = code;
+    this.num = num;
   }
 
 }
