5d621df5997d7d67a895e17518067e2b8ccb215d
210563 <210563@shinsegae.com>
2021-05-27 16:03:42 +0900

    fix: add docker and nginx

--- emart2/src/main/java/com/example/emart2/config/WebConfig.java
+++ emart2/src/main/java/com/example/emart2/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig{
       @Override
       public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:80")
             .allowCredentials(true)
         .allowedMethods(
             HttpMethod.GET.name(),
