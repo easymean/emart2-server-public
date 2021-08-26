8606b21e3a016cfd95e51a17a2b2eb1fdce78b03
easymean <m2162003@gmail.com>
2021-08-27 00:14:03 +0900

    feat: make signup

--- emart2/redirect-core/src/main/java/com/emart2/redirect/user/repository/UserRepository.java
+++ emart2/redirect-core/src/main/java/com/emart2/redirect/user/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.emart2.redirect.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-  UserEntity findByUserId(String userId);
+  UserEntity findByEmail(String email);
 }
