c28901816968212ca9367e11716f98f988852c46
easymean <m2162003@gmail.com>
2021-08-27 00:13:31 +0900

    fix: 파일이름이 동일할 때 bean이 생성되지 않는 이슈

--- emart2/redirect-api/src/main/java/com/emart2/redirect/config/WebApiConfig.java
+++ emart2/redirect-api/src/main/java/com/emart2/redirect/config/WebApiConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebApiConfig {
 
   @Bean
   public EnumMapper enumMapper() {
